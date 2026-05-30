87b5291fa5821594152ab7b1d43735aa800e0296
wisetrackdev <d.kumar@wisetrack.in>
2026-05-30 10:20:46 +0530

    ddfdfdfd

---
+++ Controllers/SizeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce_Backend.Controllers
+{
+    public class SizeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
